4144fc1dbf5fd4356d5858667e09c89c2dc09a26
superRagnarok <73694075+superRagnarok@users.noreply.github.com>
2021-04-23 20:11:40 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/sketch.js
@@ -0,0 +1,44 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+ 
+  
+  bg= loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("aladdin_cave2.jpg");
+  
+}
+
+function setup() {
+  createCanvas(1000,500);
+  security = new Security();
+  system = new System();
+  
+  
+  
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+ // text("Write Your Code in UPPERCASE", 400,80);
+// add code for changing the background to the treasure background
+  
+
+  if(score === 3) {
+    clear();
+    background(bg2)
+    strokeWeight(5);
+    stroke(255);
+    fill("RED")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+    
+  }
+
+  drawSprites()
+}
